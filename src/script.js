56836e3b497f719832dba0dccfb9e68ebc1bdd9e
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-03-05 19:40:46 +0530

    Updated dependencies

--- src/script.js
+++ src/script.js
@@ -3,20 +3,3 @@ const noContext = document.getElementById('noMenu');
 noContext.addEventListener('contextmenu', (e) => {
   e.preventDefault();
 });
-
-// SHORTCUT DISABLE START ======================================================================== //
-document.addEventListener("keydown", function(event) {
-	// Prevent default action for F5, Ctrl+R, and Ctrl+F5
-	if (event.keyCode === 116 || (event.ctrlKey && event.keyCode === 82)) {
-		event.preventDefault();
-	}
-	if (event.ctrlKey && event.keyCode === 116) {
-		event.preventDefault();
-		location.reload(true);
-	}
-	// Prevent default action for Ctrl+Shift+I
-	if (event.ctrlKey && event.shiftKey && event.keyCode === 73) {
-		event.preventDefault();
-	}
-});
-// ========================================================================== SHORTCUT DISABLE END //
