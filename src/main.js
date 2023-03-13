65dbe4f7cbe2203c8c784109412276c360b4a1fc
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-03-13 18:31:59 +0530

    Changed name "YouTube-CLI" to "SupTube"

--- src/main.js
+++ src/main.js
@@ -7,16 +7,3 @@ document.addEventListener('DOMContentLoaded', () => {
   invoke('close_splashscreen')
 })
 // ======================================= CONFIGURING SPLASHSCREEN END //
-
-
-// CONFIGURING ADGUARD START ========================================== //
-const adguard = require('adguard');
-
-adguard.init().then(() => {
-  console.log('AdGuard initialized successfully!');
-}).catch((err) => {
-  console.error('AdGuard initialization failed:', err);
-});
-
-adguard.blockUrl('https://www.youtube.com');
-// ============================================ CONFIGURING ADGUARD END //
