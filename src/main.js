fed3d0df7e4c314850c8ea4f906ba81d57b2426f
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-03-31 21:50:16 +0530

    Removed AdGuard

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { invoke } from '@tauri-apps/api/tauri'
-import './assets/adguard.js';
 const invoke = window.__TAURI__.invoke
 
 
@@ -8,16 +7,3 @@ document.addEventListener('DOMContentLoaded', () => {
   invoke('close_splashscreen')
 })
 // ======================================= CONFIGURING SPLASHSCREEN END //
-
-
-// CONFIGURING ADGUARD START ========================================== //
-const adguard = require('adguard');
-
-adguard.init().then(() => {
-  console.log('AdGuard initialized successfully!');
-}).catch((err) => {
-  console.error('AdGuard initialization failed:', err);
-});
-
-adguard.blockUrl('https://www.youtube.com');
-// ============================================ CONFIGURING ADGUARD END //
